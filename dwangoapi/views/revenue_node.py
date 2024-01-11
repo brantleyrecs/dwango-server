6cf610821f40824dcc75653a0f82d82cfa3ff3d2
Michael Brantley <brantleyrecs@gmail.com>
2024-01-10 18:22:30 -0600

    create revenue node created

--- dwangoapi/views/revenue_node.py
+++ dwangoapi/views/revenue_node.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework import serializers, status
-from dwangoapi.models import Revenue_Node
+from dwangoapi.models import Revenue_Node, Order
 
 class RevenueNodeView(ViewSet):
   def retrieve(self, request, pk):
@@ -13,6 +13,22 @@ class RevenueNodeView(ViewSet):
   def list(self, request):
     revenues = Revenue_Node.objects.all()
     serializer = RevenueSerializer(revenues, many=True)
+    return Response(serializer.data)
+  
+  def create(self, request):
+    """Create Revenue Node"""
+    order_id = Order.objects.get(pk=request.data["orderId"])
+    
+    revenue = Revenue_Node.objects.create(
+      order=order_id,
+      total_order_amount=request.data["totalOrderAmount"],
+      date_of_closure=request.data["dateOfClosure"],
+      payment_type=request.data["paymentType"],
+      tip_amount=request.data["tipAmount"],
+    )
+    
+    revenue.save()
+    serializer = RevenueSerializer(revenue)
     return Response(serializer.data)
 
 class RevenueSerializer(serializers.ModelSerializer):
