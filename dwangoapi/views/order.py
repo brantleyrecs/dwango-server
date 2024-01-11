13a622fd6a6d81932dbd6f15e7ecc4f8215d22e7
Michael Brantley <brantleyrecs@gmail.com>
2024-01-10 18:08:35 -0600

    create, update, delete orders created

--- dwangoapi/views/order.py
+++ dwangoapi/views/order.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework import serializers, status
-from dwangoapi.models import Order
+from dwangoapi.models import Order, User
 
 class OrderView(ViewSet):
   
@@ -17,10 +17,49 @@ class OrderView(ViewSet):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
+  
+  def create(self, request):
+    """Create Order"""
+    user_id = User.objects.get(pk=request.data["userId"])
+    
+    order = Order.objects.create(
+      user=user_id,
+      customer_name=request.data["customerName"],
+      phone_number=request.data["phoneNumber"],
+      email=request.data["email"],
+      order_type=request.data["orderType"],
+      status=request.data["status"],
+    )
+    
+    order.save()
+    serializer = OrderSerializer(order)
+    return Response(serializer.data)
+  
+  def update(self, request, pk):
+    """Update Order"""
+    order = Order.objects.get(pk=pk)
+    order.customer_name=request.data["customerName"]
+    order.phone_number=request.data["phoneNumber"]
+    order.email=request.data["email"]
+    order.order_type=request.data["orderType"]
+    order.status=request.data["status"]
+    
+    user_id=User.objects.get(pk=request.data["userId"])
+    order.user=user_id
+    
+    order.save()
+    serializer = OrderSerializer(order)
+    return Response(serializer.data)
+  
+  def destroy(self, request, pk):
+    """Delete Order"""
+    order = Order.objects.get(pk=pk)
+    order.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class OrderSerializer(serializers.ModelSerializer):
   """serializer for Order"""
   class Meta:
     model=Order
-    fields = ('id', 'customer_name', 'phone_number', 'email', 'order_type', 'status', 'user')
-    depth = 1
+    fields = ('id', 'customer_name', 'phone_number', 'email', 'order_type', 'status', 'user', 'items')
+    depth = 2
